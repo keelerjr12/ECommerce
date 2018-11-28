a4c9281caa91cebf2a90dd35da15dec4c5930c36
DiShen <saddiesh@umich.edu>
2018-11-28 11:40:48 -0500

    Category Products showing, Modified Home Page

--- Presentation/ECommerceWeb/Areas/Product/Pages/Index.cshtml.cs
+++ Presentation/ECommerceWeb/Areas/Product/Pages/Index.cshtml.cs
@@ -1,24 +1,50 @@
-﻿using System.Collections.Generic;
-using ECommerceApplication;
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ECommerceApplication.CartService;
+using ECommerceDomain.Sales.Product;
 
 namespace ECommerceWeb.Areas.Product.Pages
 {
-    public class ProductModel : PageModel
+    public class IndexModel : PageModel
     {
+        public List<ProductViewModel> Products { get; } = new List<ProductViewModel>();
 
-        public List<ECommerceDomain.Product.Product> Products { get; private set; }
-
-        public ProductModel(ProductService productService)
+        public IndexModel(IProductRepository productRepo, CartService cartService)
         {
-            _productService = productService;
+            _productRepo = productRepo;
+            _cartService = cartService;
         }
 
         public void OnGet()
         {
-            Products = _productService.GetAllProducts();
+            var products = _productRepo.GetAllProducts().OrderBy(product => product.Description);
+
+            foreach (var prod in products)
+            {
+                var productVM = new ProductViewModel
+                {
+                    SKU = prod.SKU,
+                    Description = prod.Description,
+                    Price = prod.Price,
+                    Category = prod.Category
+
+                };
+
+                Products.Add(productVM);
+            }
         }
 
-        private ProductService _productService;
+        public IActionResult OnPost(string sku)
+        {
+            _cartService.AddProductToCart(1, sku, 1);
+
+            return Redirect("Cart");
+        }
+
+        private IProductRepository _productRepo;
+        private CartService _cartService;
     }
 }
---
+++ Presentation/ECommerceWeb/Areas/Product/Pages/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerceWeb.Areas.Product.Pages
+{
+    public class ProductViewModel
+    {
+        public string SKU { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
