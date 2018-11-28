e14e52c9c210156a86d4cc24b22c708f27ad0e6b
Varun Patel <varunpat@umich.edu>
2018-11-27 22:55:28 -0500

    WIP...not working

--- Presentation/ECommerceWeb/Pages/Login.cshtml.cs
+++ Presentation/ECommerceWeb/Pages/Login.cshtml.cs
@@ -15,15 +15,19 @@ namespace ECommerceWeb.Pages
 
         public IActionResult OnPost(string username, string password)
         {
+            //check the user name and password in database
             if (username != "demo" || password != "demo") return RedirectToPage();
 
-            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+            //if it exsists in database, then create a user model
 
-            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
-            identity.AddClaim(new Claim(ClaimTypes.Name, username));
-            var principal = new ClaimsPrincipal(identity);
-            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal,
-                new AuthenticationProperties { IsPersistent = true });
+
+            //var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+
+            //identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
+            //identity.AddClaim(new Claim(ClaimTypes.Name, username));
+            //var principal = new ClaimsPrincipal(identity);
+            //HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal,
+            //    new AuthenticationProperties { IsPersistent = true });
 
             return RedirectToPage("/Account");
         }
