175bcff558c8b5501d51d767677aefff944c4769
keelerjr12 <keelerjr12@gmail.com>
2018-12-15 14:57:52 -0500

    Fixed product category menu issues

---
+++ Presentation/ECommerceWeb/ViewComponents/ProductCatalogMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECommerceApplication.Shopping.ProductCategory.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWeb.ViewComponents
+{
+    public class ProductCatalogMenuViewComponent : ViewComponent
+    {
+        public ProductCatalogMenuViewComponent(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _mediator.Send(new AllProductCategoriesQuery.Request()).Result.ProductCategories;
+
+            return View(categories);
+        }
+
+        private readonly IMediator _mediator;
+    }
+}
